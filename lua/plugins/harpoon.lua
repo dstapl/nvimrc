4fae86e8d90a36f0ef8b33fedbe3dcdeb5f4a4ef
Daniel Stapleton <blamblamdan@gmail.com>
2025-05-19 01:30:56 +0100

    add animation commands

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,7 +2,11 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {"<C-e>", "<leader>aa", "<leader>ad",},
+	-- keys = {
+	-- 	{"<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle harpoon UI"},
+	-- 	{"<leader>ha",function() harpoon:list():add() end, desc = "Add buffer to harpoon list"},
+	-- 	{"<leader>hd", function() harpoon:list():remove() end, desc = "Remove buffer from harpoon list"},
+	-- },
 	opts = {
 		-- TODO: Not working currently
 		-- 	general = {
@@ -10,6 +14,7 @@ return {
 		-- 		encode = false,
 		-- 	},
 	},
+	-- config = true,
 	config = function (_, opts)
 		local harpoon = require('harpoon')
 		harpoon:setup(opts)
