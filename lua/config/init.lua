c93a3f83538818cbdc8b081d193ea4ce75417c13
dstapl <22639959+dstapl@users.noreply.github.com>
2026-06-15 15:08:14 +0100

    Add transpose plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,10 @@
+vim.cmd("source " .. vim.fn.stdpath("config") ..
+	"/lua/config/transwrd.vim"
+)
+
 return {
 	require("config.options"),
 	require("config.keymaps"),
 	require("config.statusline"),
 }
+
